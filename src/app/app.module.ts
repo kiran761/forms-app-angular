34c28dadb5240d0040644004f215e5a481f44870
kiran <rkirankumar761@gmail.com>
2024-08-17 11:20:55 +0530

    Added a simple bootstrap login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
